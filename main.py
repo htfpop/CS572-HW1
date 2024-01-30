60827407b1a4d8ef2405569f37f8e188321a8a59
Christopher Leung <ckleung@usc.edu>
2024-01-30 00:40:52 -0800

    Support for write to json, and matching url/data sanitization

--- main.py
+++ main.py
@@ -25,9 +25,10 @@ BASE_URL = 'http://www.ask.com/web?q='
 
 LOGFILE = './logs/log'
 
-MAX_RETRIES = 5
+MAX_RETRIES = 10
 TIMEOUT = 30
 
+CURRENT_TIME = ""
 
 class SearchEngine:
 	def __init__(self):
@@ -37,6 +38,7 @@ class SearchEngine:
 		self.google_json = {}
 		self.urlSet = set()
 		self.file = None
+		self.json = None
 
 	def query_open(self, path):
 		with open(file=path, mode='r') as f:
@@ -53,17 +55,29 @@ class SearchEngine:
 			print(f'[ERROR]: Could not find 100 Queries in {path}\r\nExiting now..\r\n')
 			exit(-1)
 
-	def log_open(self, i = 1):
+	def log_open(self, i=1):
+		global CURRENT_TIME
 		now = datetime.now()
 		current_time = now.strftime("%m%d%Y_%H%M%S")
-		print("Current Time =", current_time)
-		self.file = open(LOGFILE+current_time+f'_i{i}'+'.txt', 'w', encoding='utf-8')
+		CURRENT_TIME = current_time
+		print("Current Time =", CURRENT_TIME)
+		self.file = open(LOGFILE + CURRENT_TIME + f'_i{i}' + '.txt', 'w', encoding='utf-8')
+
+	def json_open(self, i=1):
+		global CURRENT_TIME
+		self.json = open(LOGFILE + CURRENT_TIME + f'_i{i}' + '.json', 'w')
 
 	def log_write(self, s):
 		self.file.write(s)
 
 	def log_close(self):
 		self.file.close()
+
+	def json_write(self, s: dict):
+		json.dump(s, self.json, indent=3)
+
+	def json_close(self):
+		self.json.close()
 
 	def google_query_open(self, path):
 		with open(file=path, mode='r') as f:
@@ -190,16 +204,33 @@ def test3():
 
 def parser(e: SearchEngine):
 	# sanity check to make sure we are on the same page as google json
+	out_dict = {}
+	curr_query = 1
 	for query in e.queries:
+		query_hits = 0
+		total_url = len(e.google_json[query])
+		g_query_list = []
+		curr_query_str = f'Query {curr_query}'
+		result_list = []
+
 		# Use Selenium to parse JS
-		#chrome_options = Options()
-		#chrome_options.add_argument("--headless")
-		#driver = webdriver.Chrome(chrome_options)
-		driver = webdriver.Chrome()
+		chrome_options = Options()
+		chrome_options.add_argument("--headless")
+		# chrome_options.add_argument("--disable-notifications")
+		driver = webdriver.Chrome(chrome_options)
 		driver.set_page_load_timeout(TIMEOUT)
 
 		# Make sure the query in list is actually inside the json file
 		if len(e.google_json[query]) != 0:
+
+			# Append sanitized google query
+			for url in range(total_url):
+				g_query_list.append(url_filter(e.google_json[query][url]))
+
+			# DEBUG print out google query
+			for url in range(len(g_query_list)):
+				print(g_query_list[url])
+
 			split_query = '+'.join(query.split())
 			final_query = BASE_URL + split_query
 
@@ -221,7 +252,6 @@ def parser(e: SearchEngine):
 				print(f'Cannot scrape, Exiting...\r\n')
 				exit(-100)
 
-
 			# using BS4 to process Ask.com
 			soup = BeautifulSoup(driver.page_source, 'html.parser')
 
@@ -229,7 +259,7 @@ def parser(e: SearchEngine):
 			print(f'Results from {final_query}\r')
 			e.log_write(f'Results from {final_query}\r\n')
 
-			#urls = soup.find_all('div', class_="result-url-section")
+			# urls = soup.find_all('div', class_="result-url-section")
 			titles = soup.find_all('div', class_="result-title")
 
 			for title in titles:
@@ -237,11 +267,13 @@ def parser(e: SearchEngine):
 				link = t_title['href']
 				header = t_title['title']
 
+				sanitized_url = url_filter(link)
+
 				count2 = 0
 				# fetch actual website
 				while count2 < MAX_RETRIES:
 					try:
-						sleep_val = random.randrange(5, 10)
+						sleep_val = random.randrange(5, 15)
 						sleep(sleep_val)
 
 						driver.get(link)
@@ -249,6 +281,8 @@ def parser(e: SearchEngine):
 						# printout
 						print(f'[{sleep_val} sec]: {header} ||| {link}')
 						e.log_write(f'[{sleep_val} sec]: {header} ||| {link}\r\n')
+
+						result_list.append(link)
 
 						break
 					except TimeoutException:
@@ -258,11 +292,10 @@ def parser(e: SearchEngine):
 						driver.quit()
 
 						# Re-initialize Selenium
-						#driver = webdriver.Chrome(chrome_options)
-						driver = webdriver.Chrome()
+						driver = webdriver.Chrome(chrome_options)
 
 						# Invoke sleep to prevent IP Ban
-						sleep(random.randrange(10))
+						sleep(10)
 
 						count2 += 1
 
@@ -270,23 +303,59 @@ def parser(e: SearchEngine):
 					print(f'Cannot fetch websites, Exiting...\r\n')
 					exit(-200)
 
-				#driver.implicitly_wait(5)
+				# driver.implicitly_wait(5)
 				fetch = driver.current_url
 				if link != fetch:
 					# printout
 					e.log_write(f'[Link Redirect]: OLD: {link} ||| NEW: {fetch}\r')
 					print(f'[Link Redirect]: OLD: {link} ||| NEW: {fetch}\r')
-				#e.urlSet.add(link)
 
-			#wait = random.randrange(5, 10)
-			wait = 3
-			# printout
-			print(f'[SLEEP {wait}]')
-			e.log_write(f'[SLEEP {wait}]\r\n')
+				# Check if we have a match
+				for url in range(len(g_query_list)):
+					if g_query_list[url] == sanitized_url:
+						print(f'[MATCHED]: {g_query_list[url]} ||| {sanitized_url}\r')
+						e.log_write(f'[MATCHED]: {g_query_list[url]} ||| {sanitized_url}\r')
+						query_hits += 1
+					elif g_query_list[url] == url_filter(fetch):
+						print(f'[MATCHED]: {g_query_list[url]} ||| {fetch}\r')
+						e.log_write(f'[MATCHED]: {g_query_list[url]} ||| {fetch}\r')
+						query_hits += 1
 
-			sleep(wait)
+			driver.quit()
 
-			#driver.quit()
+			append = {curr_query_str: result_list}
+			out_dict.update(append)
+
+		print(f'Total Query Hits: {query_hits} out of {len(g_query_list)}\r')
+		e.log_write(f'Total Query Hits: {query_hits} out of {len(g_query_list)}\r')
+
+		wait = 3
+		# printout
+		print(f'[SLEEP {wait}]')
+		e.log_write(f'[SLEEP {wait}]\r\n')
+		sleep(wait)
+
+		curr_query += 1
+
+	e.json_write(out_dict)
+
+def url_filter(url: str):
+	return url.replace('https://www.', '').replace('http://www.', '').replace('https://', '').replace('http://', '').rstrip('/')
+
+
+def test4():
+	data = {
+		'query1': ['value1', 'value2', 'value3']
+	}
+
+	temp = {'query2':['value11', 'value12', 'value31']}
+
+	data.update(temp)
+
+	with open('test.json', 'w') as json_file:
+		json.dump(data, json_file, indent=3)
+
+
 
 
 if __name__ == '__main__':
@@ -300,13 +369,16 @@ if __name__ == '__main__':
 
 		engine = SearchEngine()
 		engine.log_open(x)
+		engine.json_open(x)
 		engine.query_open('./Queries/100QueriesSet3.txt')
 		# print(engine)
 		# engine.get_queries()
 		engine.google_query_open('./Queries/Google_Result3.json')
 		# engine.get_google_json()
 		parser(engine)
+		#test4()
 		engine.log_close()
+		engine.json_close()
 		# test2(engine)
 		# test3()
 
